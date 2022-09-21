8421e867999b8c2162f2d7ad1ee316a105ed39d6
Gadhiyadhruvi <113581090+Gadhiyadhruvi@users.noreply.github.com>
2022-09-21 11:16:30 +0530

    a

--- Assignment/Module 1/A19.dart
+++ Assignment/Module 1/A19.dart
@@ -2,5 +2,63 @@
 // Be Menu Driven) 
 import 'dart:io';
 void main(){
-  
+  another();
+  fun();
+}
+void another(){
+  int num;
+  print("Entet case number...");
+  num=int.parse(stdin.readLineSync()!);
+
+  switch(num){
+
+    case 1:
+      int base,height;
+      double area;
+      print("Enter base number...");
+      base=int.parse(stdin.readLineSync()!);
+      print("Enter height number...");
+      height=int.parse(stdin.readLineSync()!);
+
+      area=(base*height)/2;
+      print("Area of triangle=$area");
+      break;
+
+    case 2:
+       int r;
+       double p = 3.14, circle;
+       print("enter value r");
+       r = int.parse(stdin.readLineSync()!);
+
+       circle = 2 * p * r;
+       print("circle=$circle");
+       break;
+
+    case 3:
+       int width, height, Ans;
+       print("\nEnter value of width  : ");
+       width = int.parse(stdin.readLineSync()!);
+       print("\nEnter value of height  : ");
+       height = int.parse(stdin.readLineSync()!);
+
+       Ans = width * height;
+       print("Area of Rectangle : $Ans");
+  }
+}
+
+
+
+void fun(){
+  bool status=true;
+  String? choice;
+  while (status){
+    print("Do you want to continue more operation press Y for yes and press N for no:-");
+    choice=stdin.readLineSync()!;
+    if(choice=='y'){
+      another();
+    }else{
+      status=false;
+      break;
+    }
+  }
 }
--- Assignment/Module 1/A5.dart
+++ Assignment/Module 1/A5.dart
@@ -1,4 +1,16 @@
 // 5. write a program to find area of tringle.
 
 import 'dart:io';
+void main(){
+  int base,height;
+  double area;
 
+  print("Enter base number...");
+  base=int.parse(stdin.readLineSync()!);
+  print("Enter height number...");
+  height=int.parse(stdin.readLineSync()!);
+
+  area=(base*height)/2;
+  print("Area of triangle=$area");
+}
+    
